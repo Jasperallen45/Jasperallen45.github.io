7820a909e6b50442c9f8039e87aace574f30b79b
Jasper Allen <jasperrallen@gmail.com>
2024-07-31 14:41:27 -0500

    hgckg

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+let result = document.getElementById("GFG");
+ 
+        function changeColor(color) {
+            document.body.style.background = color;
+        }
+ 
+        function myFunc() {
+            changeColor('aqua');
+        }
+
+        function myFunc2() {
+            changeColor('black');
+        }
